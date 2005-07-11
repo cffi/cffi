510df8c24fdde906634b8c608f9eed805c62e237
Luis Oliveira <loliveira@common-lisp.net>
2005-07-11 12:53:25 +0800

    Implement DEFCFUN-HELPER idea.
     - cffi-sys implementations can now define a defcfun-helper
       function that returns two forms, a prelude and a caller.
       (todo: explain this better in the code/doc)
     - changed foreign-funcall-1 to translate-objects
       (also todo: document translate-objects)

--- src/functions.lisp
+++ src/functions.lisp
@@ -33,42 +33,46 @@
 ;;; functions.  It converts each argument based on the installed
 ;;; translators for its type, then passes the resulting list to
 ;;; CFFI-SYS:%FOREIGN-FUNCALL.
+;;;
+;;; For implementation-specific reasons, DEFCFUN doesn't use
+;;; FOREIGN-FUNCALL directly and might use something else
+;;; (passed to TRANSLATE-OBJECTS as the CALL argument) instead
+;;; of CFFI-SYS:%FOREIGN-FUNCALL to call the foreign-function.
 
-;; This macro is pretty darn hairy; patches welcome to refactor!
-(defmacro foreign-funcall-1 (name in-args done-args)
-  "Helper macro for FOREIGN-FUNCALL*."
-  (flet ((convert-type (type)
-           (canonicalize-foreign-type type))
-         (append-done-args (&rest args)
-           (append args done-args)))
-    (cond
-      ;; All arguments have been translated---perform the call.
-      ((null in-args)
-       `(%foreign-funcall ,name ,@(reverse done-args)))
-      ;; One argument remains---it is the return value.
-      ((null (cdr in-args))
-       (let ((sym (gensym))
-             (type (car in-args)))
-         `(with-object-translated
-              (,sym (foreign-funcall-1
-                     ,name nil
-                     ,(append-done-args (convert-type type)))
-                    ,type :result)
-            ,sym)))
-      ;; More than one argument is available---translate the first
-      ;; argument/type pair and recurse.
-      (t
-       (let ((sym (gensym))
-             (type (first in-args))
-             (value (second in-args)))
-         `(with-object-translated (,sym ,value ,type :in)
-            (foreign-funcall-1
-             ,name ,(cddr in-args)
-             ,(append-done-args sym (convert-type type)))))))))
+(defmacro translate-objects (syms args types rettype call)
+  "Helper macro for FOREIGN-FUNCALL and DEFCFUN."
+  (cond
+    ;; All arguments have been translated, translate
+    ;; the return value and perform the call.
+    ((null args)
+     (let ((sym (gensym)))
+       `(with-object-translated (,sym ,call ,rettype :result)
+          ,sym)))
+    ;; More than one argument is available---translate the first
+    ;; argument/type pair and recurse.
+    (t `(with-object-translated (,(car syms) ,(car args) ,(car types) :in)
+          (translate-objects
+           ,(rest syms) ,(rest args) ,(rest types) ,rettype ,call)))))
+
+(defun parse-args-and-types (args)
+  "Returns 4 values. Types, canonicalized types, args and return type."
+  (let ((return-type :void))
+    (loop for (type arg) on args by #'cddr
+          if arg collect type into types
+             and collect (canonicalize-foreign-type type) into ctypes
+             and collect arg into fargs
+          else do (setf return-type type)
+          finally (return (values types ctypes fargs return-type)))))
 
 (defmacro foreign-funcall (name &rest args)
-  "Wrapper around FOREIGN-FUNCALL that translates its arguments."
-  `(foreign-funcall-1 ,name ,args nil))
+  "Wrapper around %FOREIGN-FUNCALL that translates its arguments."
+  (multiple-value-bind (types ctypes fargs rettype)
+      (parse-args-and-types args)
+    (let ((syms (loop repeat (length fargs) collect (gensym))))
+      `(translate-objects ,syms ,fargs ,types ,rettype
+                          (%foreign-funcall ,name
+                                            ,@(mapcan #'list ctypes syms)
+                                            ,rettype)))))
 
 ;;;# Defining Foreign Functions
 ;;;
@@ -87,10 +91,40 @@
     (list (first name))
     (string name)))
 
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (when (not (find-symbol "DEFCFUN-HELPER" '#:cffi-sys))
+    (defun defcfun-helper (name rettype args types)
+      (values
+       '()
+       `(%foreign-funcall ,name ,@(mapcan #'list types args) ,rettype)))))
+
 (defmacro defcfun (name return-type &body args)
   "Defines a Lisp function that calls a foreign function."
   (let ((lisp-name (lisp-function-name name))
-        (foreign-name (foreign-function-name name)))
-    `(defun ,lisp-name ,(mapcar #'car args)
-       (foreign-funcall 
-        ,foreign-name ,@(mapcan #'reverse args) ,return-type))))
+        (foreign-name (foreign-function-name name))
+        (arg-names (mapcar #'car args))
+        (arg-types (mapcar #'cadr args))
+        (syms (loop repeat (length args) collect (gensym))))
+    (multiple-value-bind (prelude caller)
+        (defcfun-helper foreign-name return-type syms
+                        (mapcar #'canonicalize-foreign-type arg-types))
+      `(progn
+         ,prelude
+         (defun ,lisp-name ,arg-names
+           (translate-objects
+            ,syms ,arg-names ,arg-types ,return-type ,caller))))))
+
+;;;# Per-implementation DEFCFUN optimization
+
+#|
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (when (and (not (find-symbol "DEFCFUN-COMPILER-MACRO" '#:cffi-sys))
+    (defun defcfun-compiler-macro (form &rest other-args)
+      (declare (ignore other-args))
+      (format nil "compiler macro returning: ~A" form) 
+      form))))
+
+(define-compiler-macro defcfun (&whole form name return-type &body args)
+  (format nil "running compiler macro!")
+  (defcfun-compiler-macro form name return-type args))
+|#
