f2638634822ae117b4ff6608072af03ecd26b02c
Luis Oliveira <loliveira@common-lisp.net>
2005-12-23 03:39:04 +0800

    Push :cffi to *features*

--- src/package.lisp
+++ src/package.lisp
@@ -27,6 +27,9 @@
 
 (in-package #:cl-user)
 
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (pushnew :cffi *features*))
+
 (defpackage #:cffi
   (:use #:common-lisp #:cffi-sys #:cffi-utils)
   (:export
