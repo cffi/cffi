97f5d6ba90c13e06aa743e4e37a503b51b856187
Luis Oliveira <loliveira@common-lisp.net>
2006-01-21 11:38:19 +0800

    Fix foreign-funcall sprintf tests' return types.

--- tests/funcall.lisp
+++ tests/funcall.lisp
@@ -95,36 +95,36 @@
 (deftest funcall.varargs.char
     (with-foreign-pointer-as-string (s 100)
       (setf (mem-ref s :char) 0)
-      (foreign-funcall "sprintf" :pointer s :string "%c" :int 65 :void))
+      (foreign-funcall "sprintf" :pointer s :string "%c" :int 65 :int))
   "A")
 
 (deftest funcall.varargs.int
     (with-foreign-pointer-as-string (s 100)
       (setf (mem-ref s :char) 0)
-      (foreign-funcall "sprintf" :pointer s :string "%d" :int 1000 :void))
+      (foreign-funcall "sprintf" :pointer s :string "%d" :int 1000 :int))
   "1000")
 
 (deftest funcall.varargs.long
     (with-foreign-pointer-as-string (s 100)
       (setf (mem-ref s :char) 0)
-      (foreign-funcall "sprintf" :pointer s :string "%ld" :long 131072 :void))
+      (foreign-funcall "sprintf" :pointer s :string "%ld" :long 131072 :int))
   "131072")
 
-;; There is no FUNCALL.VARARGS.FLOAT as floats are promoted to double
-;; when passed as variable arguments.  Currently this fails in SBCL
-;; and CMU CL on Darwin/ppc.
+;;; There is no FUNCALL.VARARGS.FLOAT as floats are promoted to double
+;;; when passed as variable arguments.  Currently this fails in SBCL
+;;; and CMU CL on Darwin/ppc.
 (deftest funcall.varargs.double
     (with-foreign-pointer-as-string (s 100)
       (setf (mem-ref s :char) 0)
       (foreign-funcall "sprintf" :pointer s :string "%.2f"
-                       :double (coerce pi 'double-float) :void))
+                       :double (coerce pi 'double-float) :int))
   "3.14")
 
 (deftest funcall.varargs.string
     (with-foreign-pointer-as-string (s 100)
       (setf (mem-ref s :char) 0)
       (foreign-funcall "sprintf" :pointer s :string "%s, %s!"
-                       :string "Hello" :string "world" :void))
+                       :string "Hello" :string "world" :int))
   "Hello, world!")
 
 ) ;; #+cffi-features:foreign-funcall
