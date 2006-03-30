5618132b4aa86942ed3319e73cf9286cb8dbc505
Luis Oliveira <loliveira@common-lisp.net>
2006-03-31 05:50:53 +0800

    sbcl/win32: mark defcfun.undefined as an expected failure

--- tests/defcfun.lisp
+++ tests/defcfun.lisp
@@ -315,7 +315,7 @@
 ;;; regression test: defining an undefined foreign function should only
 ;;; throw some sort of warning, not signal an error.
 
-#+(or cmu (and sbcl (not linkage-table)))
+#+(or cmu (and sbcl (or (not linkage-table) win32)))
 (pushnew 'defcfun.undefined rt::*expected-failures*)
 
 (deftest defcfun.undefined
