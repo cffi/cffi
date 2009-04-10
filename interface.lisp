8f636c03e779cbb2a89ee8b0b2ee5e1305d0db0d
Liam Healy <liam@thinkpad.local>
2009-04-09 22:15:55 -0400

    Fix missing null pointer
    In #'defcstruct, the last element in the list of elements needs to be
    a null pointer.  Its absence caused strange memory problems in SBCL
    when run from SLIME.  This is now fixed.

--- interface.lisp
+++ interface.lisp
@@ -1,6 +1,6 @@
 ;; User interface for making definitions
 ;; Liam Healy 2009-04-07 22:42:15EDT interface.lisp
-;; Time-stamp: <2009-04-09 00:29:10EDT interface.lisp>
+;; Time-stamp: <2009-04-09 22:11:28EDT interface.lisp>
 ;; $Id: $
 
 (in-package :fsbv)
@@ -54,31 +54,34 @@
 (defmacro defcstruct (name-and-options &body fields)
   "A macro to define the struct to CFFI and to libffi simultaneously.
    Syntax is exactly that of cffi:defcstruct."
-  `(progn
-     (cffi:defcstruct ,name-and-options ,@fields)
-     (setf (libffi-type-pointer
-	    ,(if (listp name-and-options)
-		 (first name-and-options)
-		 name-and-options))
-	   (let ((ptr (cffi:foreign-alloc 'ffi-type))
-		 (elements (cffi:foreign-alloc
-			    :pointer
-			    :count
-			    ,(1+ (apply '+ (mapcar 'field-count fields))))))
-	     (setf
-	      ;; The elements
-	      ,@(loop for field in fields with gn = 0
-		   append
-		   (loop for fn from 0 below (field-count field)
-		      append
-		      (prog1
-			  (list
-			   `(cffi:mem-aref elements :pointer ,gn)
-			   (lookup-type (second field)))
-			(incf gn))))
-	      ;; The ffi-type
-	      (cffi:foreign-slot-value ptr 'ffi-type 'size) 0
-	      (cffi:foreign-slot-value ptr 'ffi-type 'alignment) 0
-	      (cffi:foreign-slot-value ptr 'ffi-type 'type) +type-struct+
-	      (cffi:foreign-slot-value ptr 'ffi-type 'elements) elements)
-	     ptr))))
+  (let ((total-number-of-elements (apply '+ (mapcar 'field-count fields))))
+    `(progn
+       (cffi:defcstruct ,name-and-options ,@fields)
+       (setf (libffi-type-pointer
+	      ,(if (listp name-and-options)
+		   (first name-and-options)
+		   name-and-options))
+	     (let ((ptr (cffi:foreign-alloc 'ffi-type))
+		   (elements (cffi:foreign-alloc
+			      :pointer
+			      :count
+			      ,(1+ total-number-of-elements))))
+	       (setf
+		;; The elements
+		,@(loop for field in fields with gn = 0
+		     append
+		     (loop for fn from 0 below (field-count field)
+			append
+			(prog1
+			    (list
+			     `(cffi:mem-aref elements :pointer ,gn)
+			     (lookup-type (second field)))
+			  (incf gn))))
+		(cffi:mem-aref elements :pointer ,total-number-of-elements)
+		(cffi:null-pointer)
+		;; The ffi-type
+		(cffi:foreign-slot-value ptr 'ffi-type 'size) 0
+		(cffi:foreign-slot-value ptr 'ffi-type 'alignment) 0
+		(cffi:foreign-slot-value ptr 'ffi-type 'type) +type-struct+
+		(cffi:foreign-slot-value ptr 'ffi-type 'elements) elements)
+	       ptr)))))
