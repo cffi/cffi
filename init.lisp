5e058b7b08e48b7950053584afe76652be36bd5b
Liam Healy <liam@thinkpad.local>
2009-08-22 16:25:37 -0400

    Add include path for OSX
    Darwin ports installs .h files under /opt/local/include/, so put -I
    option onto cffi-grovel::*cc-flags*.

--- init.lisp
+++ init.lisp
@@ -1,6 +1,6 @@
 ;; Declaration of package
 ;; Liam Healy 2009-02-22 09:55:45EST pkgdcl.lisp
-;; Time-stamp: <2009-05-12 15:27:21EDT init.lisp>
+;; Time-stamp: <2009-08-22 16:20:00EDT init.lisp>
 
 (in-package :common-lisp-user)
 
@@ -11,5 +11,11 @@
 (cffi:load-foreign-library
  #+darwin "libffi.dylib"
  #+(and (not darwin) unix) "libffi.so")
+
+;;; When installed through Mac Ports, GSL .h files will be found
+;;; in /opt/local/include.
+#+darwin
+(setf cffi-grovel::*cc-flags*
+      (append '("-I" "/opt/local/include/") cffi-grovel::*cc-flags*))
 
 (pushnew :fsbv *features*)
