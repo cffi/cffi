2f69941d674f225e1cfd60f1fc896a60283da096
Timo Myyrä <timo.myyra@iki.fi>
2014-02-20 22:16:05 +0200

    Change to use explicit version number
    Best use explicit number in case of major library bump as suggested in http://lispcaveats.tumblr.com/post/13259176455/ffi-linking-against-shared-libraries

--- libffi/init.lisp
+++ libffi/init.lisp
@@ -30,8 +30,9 @@
 (define-foreign-library (libffi)
   (:darwin (:or "libffi.dylib" "libffi32.dylib" "/usr/lib/libffi.dylib"))
   (:solaris (:or "/usr/lib/amd64/libffi.so" "/usr/lib/libffi.so"))
+  (:openbsd (:or "libffi.so.0.0"))
   (:unix (:or "libffi.so.6" "libffi32.so.6" 
-              "libffi.so.5" "libffi32.so.5" "libffi.so"))
+              "libffi.so.5" "libffi32.so.5"))
   (:windows (:or "libffi-6.dll" "libffi-5.dll" "libffi.dll"))
   (t (:default "libffi")))
 
