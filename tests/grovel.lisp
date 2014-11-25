ef07c557a255b3981679afcbb13bb6ff810060b4
Mark Cox <markcox80@gmail.com>
2014-11-25 21:18:46 +1000

    Test case for bug 1395242.

--- tests/grovel.lisp
+++ tests/grovel.lisp
@@ -31,3 +31,35 @@
     (cffi-grovel::%invoke "echo" '("test"))
   0 "test
 ")
+
+(defun bug-1395242-helper (enum-type base-type)
+  (check-type enum-type (member constantenum cenum))
+  (check-type base-type (member :int :unsigned-int))
+  (uiop/stream:with-temporary-file (:stream grovel-stream :pathname grovel-file)
+    ;; Write the grovel file
+    (with-open-stream (*standard-output* grovel-stream)
+      (write `(,enum-type (bug-1395242-enum :base-type ,base-type)
+                          ((:max-uint32 "UINT_LEAST32_MAX")))))
+    ;; Get the value of :inaddr-broadcast
+    (let ((lisp-file (cffi-grovel:process-grovel-file grovel-file)))
+      (unwind-protect
+           (progn
+             (load lisp-file)
+             (cffi:foreign-enum-value 'bug-1395242-enum :max-uint32))
+        (uiop/filesystem:delete-file-if-exists lisp-file)))))
+
+(deftest bug-1395242.constantenum.int
+    (bug-1395242-helper 'constantenum :int)
+  -1)
+
+(deftest bug-1395242.constantenum.unsigned-int
+    (bug-1395242-helper 'constantenum :unsigned-int)
+  4294967295)
+
+(deftest bug-1395242.cenum.int
+    (bug-1395242-helper 'cenum :int)
+  -1)
+
+(deftest bug-1395242.cenum.unsigned-int
+    (bug-1395242-helper 'cenum :unsigned-int)
+  4294967295)
