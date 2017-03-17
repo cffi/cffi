dba51976ab65ae2c5b5ae4eca454099c689b7f00
Masataro Asai <guicho2.71828@gmail.com>
2017-03-18 02:59:24 +0900

    fixup! append a url link to the corresponding manual page to each docstring of exported symbols

--- src/doc.lisp
+++ src/doc.lisp
@@ -5,7 +5,7 @@
   (defun append-url (sym doc-type)
     (setf (documentation sym doc-type)
           (with-output-to-string (s)
-            (princ (documentation sym doc-type) :stream s)
+            (princ (documentation sym doc-type) s)
             ;; do the same name-normalization as texinfo
             (format s "~2% Detailed documentation: ~
                         https://common-lisp.net/project/cffi/manual/html_node/~{~a~^_002d~}.html"
