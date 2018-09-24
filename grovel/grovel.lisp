4e9cd72d61f10bedcd1d4a11779c0023f38b7cdd
sdilts <stuart.dilts@gmail.com>
2018-09-24 15:17:30 -0600

    Add new groveller directive: FEATURE
    Add item to cl:*features* (or another list) if the given name is known
    to the C preprocessor. Intended to be used when a C library defines
    preprocessor constants if it includes a certain feature.

--- grovel/grovel.lisp
+++ grovel/grovel.lisp
@@ -354,6 +354,16 @@ int main(int argc, char**argv) {
   (dotimes (i (length c-names))
     (format out "~&#endif~%")))
 
+(define-grovel-syntax feature (lisp-feature-name c-name &key (feature-list 'cl:*features*))
+  (c-section-header out "feature" lisp-feature-name)
+  (format out "~&#ifdef ~A~%" c-name)
+  (c-format out "(cl:pushnew '")
+  (c-print-symbol out lisp-feature-name t)
+  (c-format out " ")
+  (c-print-symbol out feature-list)
+  (c-format out ")~%")
+  (format out "~&#endif~%"))
+
 (define-grovel-syntax cunion (union-lisp-name union-c-name &rest slots)
   (let ((documentation (when (stringp (car slots)) (pop slots))))
     (c-section-header out "cunion" union-lisp-name)
