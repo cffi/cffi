3c015c4a879918d7b7b181459b198e45cc791286
SongWei <leo_songwei@outlook.com>
2021-03-23 23:50:39 +1100

    test

--- grovel/common.h
+++ grovel/common.h
@@ -16,6 +16,7 @@
 #define indirect_stringify(x) stringify(x)
 
 #define TYPE_SIGNED_P(type) (((type)-1)<0LL)
+#define TYPE_FLOAT_P(type) (0.4 < ((type)0.5) && ((type)0.5 < 0.6))
 #define _64_BIT_VALUE_FITS_SIGNED_P(value) ( (value) <= 0x7FFFFFFFFFFFFFFFLL )
 #define SLOT_SIGNED_P(result, type, slot)				\
   do { 									\
@@ -23,15 +24,31 @@
     slot_signed_p_struct.slot = -1; 					\
     (result) = slot_signed_p_struct.slot < 0;				\
   } while (0)
+#define SLOT_FLOAT_P(result, type, slot)                                          \
+  do {                                                                            \
+    type slot_float_p_struct;                                                     \
+    slot_float_p_struct.slot = 0.5;                                               \
+    (result) = 0.4 < slot_float_p_struct.slot && slot_float_p_struct.slot < 0.6;  \
+  } while (0)
 
-void type_name(FILE *output, int signed_p, int size) {
-  if (signed_p) {
+void type_name(FILE *output, int signed_p, int float_p, int size) {
+  if (signed_p && !float_p) {
     switch (size) {
     case 1: fprintf(output, ":int8"); break;
     case 2: fprintf(output, ":int16"); break;
     case 4: fprintf(output, ":int32"); break;
     case 8: fprintf(output, ":int64"); break;
     default: goto error;
+    }
+  } else if (signed_p && float_p) {
+    if (size == 4) {
+      fprintf(output, ":float");
+    } else if (size == 8) {
+      fprintf(output, ":double");
+    } else if (size > 8) {
+      fprintf(output, ":long-double");
+    } else {
+      goto error;
     }
   } else {
     switch(size) {
