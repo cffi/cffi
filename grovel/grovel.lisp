eac416d11bb483c33c277d164b4c6daccd9af452
Luke Gorrie <luke@nuddy.co>
2021-09-22 11:20:48 +0200

    grovel: Error message for IN-PACKAGE with bad package designator
    Report a bad package designator in (IN-PACKAGE :FOO) as
    
        The name :FOO does not designate any package.
    
    instead of
    
        The value
          NIL
        is not of type
          PACKAGE

--- grovel/grovel.lisp
+++ grovel/grovel.lisp
@@ -202,7 +202,10 @@ int main(int argc, char**argv) {
                        (flag (warn "Groveler clause FLAG is deprecated, use CC-FLAGS instead.")))
                      (case (form-kind f)
                        (in-package
-                        (setf *package* (find-package (second f)))
+                        (setf *package*
+                              (or (find-package (second f))
+                                  (error "The name ~S does not designate any package."
+                                         (second f))))
                         (push f forms))
                        (progn
                          ;; flatten progn forms
