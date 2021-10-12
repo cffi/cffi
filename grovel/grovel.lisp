430f04ccdf227c6d3eaa79613a5cde9fd1e6ca64
ghollisjr <ghollisjr@gmail.com>
2021-10-11 20:17:04 -0400

    Fixed link-executable for new GCC (--as-needed), added pkg-config-libs and ld-exe-flags grovel syntax to allow library controls in grovel files.

--- grovel/grovel.lisp
+++ grovel/grovel.lisp
@@ -299,6 +299,29 @@ int main(int argc, char**argv) {
                 (t
                  (grovel-error "~a" message))))))))
 
+(define-grovel-syntax ld-exe-flags (&rest flags)
+  (appendf *ld-exe-flags* (parse-command-flags-list flags)))
+
+(define-grovel-syntax pkg-config-libs (pkg &key optional)
+  (let ((output-stream (make-string-output-stream))
+        (program+args (list "pkg-config" pkg "--libs")))
+    (format *debug-io* "~&;~{ ~a~}~%" program+args)
+    (handler-case
+        (progn
+          (run-program program+args
+                       :output (make-broadcast-stream output-stream *debug-io*)
+                       :error-output output-stream)
+          (appendf *ld-exe-flags*
+                   (parse-command-flags (get-output-stream-string output-stream))))
+      (error (e)
+        (let ((message (format nil "~a~&~%~a~&"
+                               e (get-output-stream-string output-stream))))
+          (cond (optional
+                 (format *debug-io* "~&; ERROR: ~a" message)
+                 (format *debug-io* "~&~%; Attempting to continue anyway.~%"))
+                (t
+                 (grovel-error "~a" message))))))))
+
 ;;; This form also has some "read time" effects. See GENERATE-C-FILE.
 (define-grovel-syntax in-package (name)
   (c-format out "(cl:in-package #:~A)~%~%" name))
