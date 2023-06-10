c42b42e93a784c9396047078eff9caed257d7c28
Gleefre <varedif.a.s@gmail.com>
2023-06-10 16:49:54 +0200

    Workaround for defcfun.undefined test on CMUCL (#361)
    * Fix defcfun.undefined test on CMUCL (see https://gitlab.common-lisp.net/cmucl/cmucl/-/issues/74)
    * Remove defcfun.undefined from expected-failures on CMUCL

--- src/functions.lisp
+++ src/functions.lisp
@@ -235,6 +235,7 @@ arguments and does type promotion for the variadic arguments."
       `(progn
          ,prelude
          (defun ,lisp-name ,arg-names
+           #+cmucl (declare (notinline alien::%heap-alien))
            ,@(ensure-list docstring)
            ,(if call-by-value
                 `(foreign-funcall
